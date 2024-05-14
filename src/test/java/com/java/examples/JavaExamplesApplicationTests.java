f574386687b04b31875c7066279f76ea61616de0
Lokesh Varshney <lokeshvarshney@Lokeshs-Laptop.local>
2024-05-14 12:07:23 +0530

    Initial commit

---
+++ src/test/java/com/java/examples/JavaExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
